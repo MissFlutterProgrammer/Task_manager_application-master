75bf91d658a038af563c512ad2a639ab2ccad628
rezaiemehrnegar <rezaiemehrnegar@gmail.com>
2025-05-03 19:26:00 +0330

    5/3/2025

--- lib/utils/font_sizes.dart
+++ lib/utils/font_sizes.dart
@@ -1,6 +1,6 @@
-const double textTiny = 10.0;
-const double textSmall = 12.0;
-const double textMedium = 14.0;
-const double textExtraLarge = 18.0;
-const double textXExtraLarge = 20.0;
-const double textBold = 30.0;
+const double textTiny = 10;
+const double textSmall = 12;
+const double textMedium = 14;
+const double textExtraLarge = 18;
+const double textXExtraLarge = 20;
+const double textBold = 30;
